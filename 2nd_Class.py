db2b37b79678e7ce2dc1bf1bdf07f67aa2642fa8
Md.Sabbir Hossain <sabbirhossain76765@gmail.com>
2026-05-06 00:02:47 +0600

    Update 2nd_Class.py

--- 2nd_Class.py
+++ 2nd_Class.py
@@ -1,1 +1,23 @@
-print("hhello")
+#keyword and function len ()
+import keyword
+x=keyword.kwlist
+print(x)
+
+import keyword
+l=len(keyword.kwlist)
+print(l)
+
+#id()---->function
+
+x=5
+y=5.0
+p=id(x)
+a=id(y)
+print(p)
+print(a)
+
+#string k int a float a convert
+w="123"
+w=int(w)
+print(type(w))
+print(w)
