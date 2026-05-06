4f8fc914e96216eee0897c7fc322a63c0dcfcf5a
Md.Sabbir Hossain <sabbirhossain76765@gmail.com>
2026-05-06 10:00:26 +0600

    Add files via upload

---
+++ addition,area of circle,conditional statement.py
@@ -0,0 +1,31 @@
+#Addition Code
+
+print("Enter First Number:")
+x=input()
+print("Enter Second Number:")
+y=input()
+z=int(x)+int(y)
+print("Addition is:",z)
+
+
+#Addition short Code
+
+x=int(input("Enter First Number:"))
+y=int(input("Enter Second Number:"))
+print("Addition is:",x+y)
+
+
+#Area of Circle
+    #Formula: 3.1416*r*r
+
+r=float(input("Enter a radius:"))
+print("Area of circle is:",3.14*r*r)
+
+
+#Conditional Statements+Operator
+
+a=-7
+if a>0:
+    print("yes its true")
+else:
+    print("its false")
